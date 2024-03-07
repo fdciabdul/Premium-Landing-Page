195e5c9231658c7addd193566e8180b902b19bd6
fdciabdul <abdulmuttaqin456@gmail.com>
2024-03-08 04:20:28 +0700

    push

---
+++ spacemax/spacemax-html-v1.0/Documentation/js/src/plugins/switcher.js
@@ -0,0 +1,60 @@
+(function ($) {
+    "use strict";
+
+    $(".toggler__btn").on("click", function () {
+            $(".switcher").toggleClass("switcher__is-open")
+        }),
+            $('body').on("click", function (e) {
+                $(".switcher").removeClass("switcher__is-open");
+            });
+            $('body').on('click', '.toggler__btn', function (e) {
+                e.stopPropagation();
+            });
+            $('body').on('click', '.switcher-wrapper', function (t) {
+                t.stopPropagation();
+            });
+            
+        $(".tab-color").on("click", function () {
+            $(".switcher-gradient").removeClass("active"),
+                $(".switcher-color").addClass("active"),
+                $(".tab-gradient").removeClass("active"),
+                $(this).addClass("active")
+        }),
+        $(".tab-gradient").on("click", function () {
+            $(".switcher-color").removeClass("active"),
+                $(".switcher-gradient").addClass("active"),
+                $(".tab-color").removeClass("active"),
+                $(this).addClass("active")
+        }),
+
+        $(".switcher-tab__color button").on("click", function () {
+            var t = $(".tab-color-clone").find(".active");
+            0 < t.length && $("body").removeClass(t.attr("data-theme")),
+
+                $("body").addClass($(this).attr("data-theme")),
+                $(".switcher-tab__color button").removeClass("active"),
+                $(this).addClass("active")
+        }),
+        $(".tab-full").on("click", function () {
+            $(".switcher-boxed").removeClass("active"),
+                $("body").removeClass("boxed-layout"),
+                $(".tab-boxed").removeClass("active"),
+                $(this).addClass("active")
+        }),
+        $(".tab-boxed").on("click", function () {
+            $(".switcher-boxed").addClass("active"),
+                $("body").addClass("boxed-layout"),
+                $(".tab-full").removeClass("active"),
+                $(this).addClass("active")
+        }),
+
+        $(".switcher-tab__pattern button").on("click", function () {
+            var t = $(".switcher-boxed").find(".active");
+            0 < t.length && $("body").removeClass(t.attr("data-theme")),
+                $("body").addClass($(this).attr("data-theme")),
+                $(".switcher-tab__pattern button").removeClass("active"),
+                $(this).addClass("active")
+        })
+
+})(jQuery);
+
